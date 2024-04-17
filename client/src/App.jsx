50e10dac32d06c45a07017b2557e58f260588a9f
iamrupnath <change.01life@gmail.com>
2024-04-17 12:39:07 +0530

    Create Header Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,15 @@ import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
-import "./App.css";
+import Header from './components/Header';
+
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
        <BrowserRouter>
+       <Header />
        <Routes>
        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
