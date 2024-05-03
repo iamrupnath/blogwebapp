892718d3b7162b323373d0509e01eb817f582c5a
iamrupnath <change.01life@gmail.com>
2024-05-03 21:50:50 +0530

    Show users to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import DashSidebar from "../components/DashSidebar";
-import DashProfile from '../components/DashProfile';
-import DashPosts from '../components/DashPosts';
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+import DashUsers from '../components/DashUsers';
 
 const Dashboard = () => {
   const location = useLocation();
@@ -16,16 +17,18 @@ const Dashboard = () => {
   }, [location.search]);
   return (
     <>
-     <div className="min-h-screen flex flex-col md:flex-row">
-      <div className='md:w-56'>
-        {/* Sidebar */}
-        <DashSidebar />
+      <div className="min-h-screen flex flex-col md:flex-row">
+        <div className="md:w-56">
+          {/* Sidebar */}
+          <DashSidebar />
+        </div>
+        {/* profile... */}
+        {tab === "profile" && <DashProfile />}
+        {/* posts... */}
+        {tab === "posts" && <DashPosts />}
+        {/* users */}
+        {tab === "users" && <DashUsers />}
       </div>
-      {/* profile... */}
-      {tab === 'profile' && <DashProfile />}
-      {/* posts... */}
-      {tab === 'posts' && <DashPosts />}
-    </div>
     </>
   );
 };
