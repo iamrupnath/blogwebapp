eb80cab881febfa9b8b3bcd17db8a91f509f9d16
iamrupnath <change.01life@gmail.com>
2024-05-04 20:58:34 +0530

    Add edit functionality to the comment component

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getPostComments } from '../controllers/comment.controller.js';
+import { createComment, getPostComments, editComment } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
-
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/editComment/:commentId', verifyToken, editComment);
+
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -51,6 +51,15 @@ const CommentSection = () => {
     };
     getComments();
   }, [postId]);
+
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
+
   return (
     <>
       <div className="max-w-2xl mx-auto w-full p-3">
@@ -115,7 +124,7 @@ const CommentSection = () => {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
           ))}
         </>
       )}
